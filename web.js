1f902281f35bdba5b901fad9b41ccb64c1b4e16d
Peter Stensmyr <peter.stensmyr@gmail.com>
2013-07-16 09:38:16 +1000

    reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require("fs");
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 3!');
+  var result = fs.readFileSync("index.html", "utf-8")
+  response.send(result);
 });
 
 var port = process.env.PORT || 5000;
